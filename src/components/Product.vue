dd916e830886b6b6974c7336bae1a22795011498
oviliansyah11 <oviliansyah11@gmail.com>
2021-12-16 23:02:39 +0700

    search button

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,43 +4,55 @@
       <h1>What Else Do You Order Now</h1>
       <h3>Order now and we are ready to deliver to your destination address</h3>
     </div>
-    <div class="menu">
-      <div class="row">
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img src="img/all-menu.png" height="40px" alt="" /> All Menu
-          </button>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img src="img/donuts.png" height="40px" alt="" /> Donuts
-          </button>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img src="img/dessert.png" height="40px" alt="" /> Dessert
-          </button>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img
-              src="img/coffee.png"
-              height="40px"
-              style="margin-left: -3px"
-              alt=""
-            />
-            Hot Drinks
-          </button>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img src="img/cup.png" height="40px" alt="" /> Cold Drinks
-          </button>
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn">
-            <img src="img/discount.png" height="40px" alt="" /> Discount
-          </button>
+    <div class="row mb-5">
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img src="img/all-menu.png" height="40px" alt="" /> All Menu
+        </button>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img src="img/donuts.png" height="40px" alt="" /> Donuts
+        </button>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img src="img/dessert.png" height="40px" alt="" /> Dessert
+        </button>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img
+            src="img/coffee.png"
+            height="40px"
+            style="margin-left: -3px"
+            alt=""
+          />
+          Hot Drinks
+        </button>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img src="img/cup.png" height="40px" alt="" /> Cold Drinks
+        </button>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn">
+          <img src="img/discount.png" height="40px" alt="" /> Discount
+        </button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="input-group">
+          <div class="input-group-text bg-transparent">
+            <i class="fas fa-search"></i>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Find your menu here.."
+          />
         </div>
       </div>
     </div>
